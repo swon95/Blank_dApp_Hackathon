f369e3c7ae1732a15dfcbf7872f4893ab2e34a50
swon95 <shimtkddnjs13@gmail.com>
2023-11-23 09:45:00 +0900

    style: tailwind css modify

---
+++ client/src/components/pages/Bottom.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Icon } from "@iconify/react";
+
+const Bottom = () => {
+    const [activeNav, setActiveNav] = useState(1);
+    const onClick = () => setActiveNav(!activeNav);
+
+    return (
+        <nav className="w-full flex justify-evenly fixed bottom-0 mb-4">
+            {/* 하단 네비게이션 최상위 태그 */}
+            <div className="w-1/4 flex justify-center flex-col items-center shrink-0">
+                <Icon className="homebtn" icon={"mdi-light:home"} />
+                <p>Home</p>
+            </div>
+            {/* 네비게이션을 구성하고 있는 하나의 버튼 */}
+            <div className="w-1/4 flex jutify-center flex-col items-center shrink-0">
+                <Icon icon="mdi-light:grid" />
+                <p>Feed</p>
+            </div>
+            <div className="w-1/4 flex justify-center flex-col items-center shrink-0">
+                <Icon icon="mdi-light:plus-box" />
+                <p>New</p>
+            </div>
+            <div className="w-1/4 flex justify-center flex-col items-center shrink-0">
+                <Icon icon="mdi-light:account" />
+                <p>MyPage</p>
+            </div>
+        </nav>
+    );
+};
+
+export default Bottom;
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Icon } from "@iconify/react";
+import Bottom from "../pages/Bottom";
 
 function Home() {
-    return <div>Home</div>;
+    return (
+        <>
+            <div className="flex border-2 p-4">
+                <div className="header">
+                    <Icon icon="mdi-light:magnify" />
+                    <Icon icon="mdi-light:menu" />
+                </div>
+            </div>
+
+            <div className="body h-[1900]">{/* <ImgSlide /> */}</div>
+
+            <div className="bottom">
+                <Bottom />
+            </div>
+        </>
+    );
 }
 
 export default Home;
