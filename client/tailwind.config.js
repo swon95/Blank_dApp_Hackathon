c4f681a0fdd003e64ea8395eacd97f68c1b42a14
swon95 <shimtkddnjs13@gmail.com>
2023-11-24 12:30:18 +0900

    feat: Post & Feed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
             light20: "#CCEFEF",
             light60: "#66D0CE",
             light100: "#00B0AD",
+            profileGray: "#D9D9D9",
+            dashboardGray: "#9FA9B3",
             default: "#0F0F0F",
             descripton: "#CCEFEF",
             gray: "#E9ECEF",
