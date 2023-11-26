a984c5288386f4e89fb157d994e8e4646d4e49bf
swon95 <shimtkddnjs13@gmail.com>
2023-11-27 00:25:33 +0900

    feat: Home ImgSlider components

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -2,150 +2,141 @@ import React from "react";
 import { Icon } from "@iconify/react";
 import Bottom from "../common/Bottom";
 import ImgSlide from "./../common/ImgSlide";
+import ImgSlide2 from "../common/ImgSlide2";
 
 const dummydata = [
-    {
-        title: "Javachip Frapuccino vs Ice Vanilla Latte",
-        writer: "dsasvsd.lens",
-    },
-    {
-        title: "asbsfafsb vs Ice safbfas fsabasf",
-        writer: "adbfadad.lens",
-    },
-    {
-        title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
-        writer: "asdgasdg.lens",
-    },
-    {
-        title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
-        writer: "asdgasdg.lens",
-    },
-    {
-        title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
-        writer: "asdgasdg.lens",
-    },
-    {
-        title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
-        writer: "asdgasdg.lens",
-    },
-    {
-        title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
-        writer: "asdgasdg.lens",
-    },
+  {
+    title: "Javachip Frapuccino vs Ice Vanilla Latte",
+    writer: "dsasvsd.lens",
+  },
+  {
+    title: "asbsfafsb vs Ice safbfas fsabasf",
+    writer: "adbfadad.lens",
+  },
+  {
+    title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
+    writer: "asdgasdg.lens",
+  },
+  {
+    title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
+    writer: "asdgasdg.lens",
+  },
+  {
+    title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
+    writer: "asdgasdg.lens",
+  },
+  {
+    title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
+    writer: "asdgasdg.lens",
+  },
+  {
+    title: "asfbafdba afbadf vs Ice fadbafdb afbsa",
+    writer: "asdgasdg.lens",
+  },
 ];
 
 function Home() {
-    return (
-        <>
-            <div className="flex justify-between border-b-4 border-lightgray-500">
-                <div className="p-4 text-3xl font-bold">Blank</div>
-                <div className="flex p-4 text-3xl mt-2 ">
-                    <Icon icon="mdi-light:magnify" className="mr-2" />
-                    <Icon icon="mdi-light:menu" />
-                </div>
-            </div>
+  return (
+    <>
+      <div className="flex justify-between border-b-4 border-lightgray-500">
+        <div className="p-4 text-3xl font-bold">Blank</div>
+        <div className="flex p-4 text-3xl mt-2 ">
+          <Icon icon="mdi-light:magnify" className="mr-2" />
+          <Icon icon="mdi-light:menu" />
+        </div>
+      </div>
 
-            <div className="body w-[390] h-[1900]">
-                <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
-                    Suggested survey for you
-                </div>
-                <div className="ml-4">
-                    <ImgSlide />
-                </div>
-                <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
-                    New trending 🔥
-                </div>
-                <div className="ml-4">
-                    <ImgSlide />
-                </div>
+      <div className="body w-[390] h-[1900]">
+        <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
+          Suggested survey for you
+        </div>
+        <div className="pl-4">
+          <ImgSlide />
+        </div>
+        <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
+          New trending 🔥
+        </div>
+        <div className="pl-4">
+          <ImgSlide2 />
+        </div>
 
-                <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
-                    148 Blankers are now answering
-                </div>
-                <div className="flex">
-                    <div className="bg-pink h-24 w-72 ml-4">
-                        <div className="text-xl text-left font-bold ml-2">
-                            Javachip Frapuccino vs Ice Vanilla Latte
-                        </div>
-                        <div className="flex mr-auto ml-2 mt-1.5">
-                            <div className="w-5 h-5 bg-white rounded-full"></div>
-                            <div className="ml-1 text-bold">starbucks.lens</div>
-                        </div>
-                    </div>
-                    <div className="mx-auto mb-3">
-                        <Icon
-                            icon="mdi-light:arrow-right"
-                            color="#ccc"
-                            className="w-12 h-12 mx-auto"
-                        />
-                        <div className="text-lg text-gray2 text-center">
-                            Let's do this
-                        </div>
-                    </div>
-                </div>
-                <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
-                    Wanna see what others do?
-                </div>
-                <div className="flex">
-                    <div
-                        className="bg-gray h-36 mx-4"
-                        style={{ width: "22.3rem" }}
-                    >
-                        <div className="flex mr-auto ml-2 mb-1 mt-1.5">
-                            <div className="w-5 h-5 bg-white rounded-full"></div>
-                            <div className="ml-1 text-sm">noul.lens</div>
-                        </div>
-                        <div className="flex">
-                            {/* 아랫부분 나중에 다시 보기 */}
-                            <div className="text-left ml-2 w-60 overflow-hidden overflow-ellipsis max-h-24">
-                                This test is insane!!<br></br> You guys should
-                                really try. I got ‘Lazy Rabbit’ and the NFT is
-                                so cute so it blah blah blah blah blah blah blah
-                                blah blah blah blah blah blah blah
-                            </div>
-                            <div className="h-24 w-24 bg-white"></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
-                    All we have for you
-                </div>
-                <div className="flex justify-between mx-4">
-                    <div>Filter</div>
-                    <div>Order by</div>
-                </div>
-                {dummydata.map((data, index) => {
-                    return (
-                        <div
-                            className={`${
-                                index % 3 === 0
-                                    ? "bg-pink"
-                                    : index % 3 === 1
-                                    ? "bg-index1"
-                                    : "bg-index2 text-white"
-                            } h-24 ml-4 my-3 p-2 flex justify-between flex-col`}
-                            style={{ width: "22.3rem" }}
-                            key={index}
-                        >
-                            <div className=" text-left font-bold ml-2">
-                                {data.title}
-                            </div>
-                            <div className="flex mr-auto ml-2">
-                                <div className="w-5 h-5 bg-white rounded-full"></div>
-                                <div className="ml-1 text-bold">
-                                    {data.writer}
-                                </div>
-                            </div>
-                        </div>
-                    );
-                })}
+        <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
+          148 Blankers are now answering
+        </div>
+        <div className="flex">
+          <div className="bg-pink h-24 w-72 ml-4">
+            <div className="text-xl text-left font-bold ml-2">
+              Javachip Frapuccino vs Ice Vanilla Latte
             </div>
+            <div className="flex mr-auto ml-2 mt-1.5">
+              <div className="w-5 h-5 bg-white rounded-full"></div>
+              <div className="ml-1 text-bold">starbucks.lens</div>
+            </div>
+          </div>
+          <div className="mx-auto mb-3">
+            <Icon
+              icon="mdi-light:arrow-right"
+              color="#ccc"
+              className="w-12 h-12 mx-auto"
+            />
+            <div className="text-lg text-gray2 text-center">Let's do this</div>
+          </div>
+        </div>
+        <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
+          Wanna see what others do?
+        </div>
+        <div className="flex">
+          <div className="bg-gray h-36 mx-4" style={{ width: "22.3rem" }}>
+            <div className="flex mr-auto ml-2 mb-1 mt-1.5">
+              <div className="w-5 h-5 bg-white rounded-full"></div>
+              <div className="ml-1 text-sm">noul.lens</div>
+            </div>
+            <div className="flex">
+              {/* 아랫부분 나중에 다시 보기 */}
+              <div className="text-left ml-2 w-60 overflow-hidden overflow-ellipsis max-h-24">
+                This test is insane!!<br></br> You guys should really try. I got
+                ‘Lazy Rabbit’ and the NFT is so cute so it blah blah blah blah
+                blah blah blah blah blah blah blah blah blah blah
+              </div>
+              <div className="h-24 w-24 bg-white"></div>
+            </div>
+          </div>
+        </div>
+        <div className="p-4 pb-2 text-xl font-bold mr-auto text-left">
+          All we have for you
+        </div>
+        <div className="flex justify-between mx-4">
+          <div>Filter</div>
+          <div>Order by</div>
+        </div>
+        {dummydata.map((data, index) => {
+          return (
+            <div
+              className={`${
+                index % 3 === 0
+                  ? "bg-pink"
+                  : index % 3 === 1
+                  ? "bg-index1"
+                  : "bg-index2 text-white"
+              } h-24 ml-4 my-3 p-2 flex justify-between flex-col`}
+              style={{ width: "22.3rem" }}
+              key={index}
+            >
+              <div className=" text-left font-bold ml-2">{data.title}</div>
+              <div className="flex mr-auto ml-2">
+                <div className="w-5 h-5 bg-white rounded-full"></div>
+                <div className="ml-1 text-bold">{data.writer}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
 
-            <div className="bottom">
-                <Bottom />
-            </div>
-        </>
-    );
+      <div className="bottom">
+        <Bottom />
+      </div>
+    </>
+  );
 }
 
 export default Home;
